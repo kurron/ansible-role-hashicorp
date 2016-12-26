d2a151ca22df50051bd40acb868e22c2028730c8
Ron Kurr <kurr@kurron.org>
2016-12-25 23:50:26 -0500

    Consul agent running in development mode under systemd is working.

--- tests/run-test.sh
+++ tests/run-test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible-playbook --verbose --inventory-file inventory --user vagrant --ask-pass --become-user root --ask-become-pass --tags consul-daemon test.yml
+ansible-playbook --tags consul-daemon --verbose --inventory-file inventory --user vagrant --ask-pass --become-user root --ask-become-pass --tags consul-daemon test.yml
